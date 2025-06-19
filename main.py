375a5ec6a3d42f00b5da9de05286e6177edb4b23
Madiyar0101 <iiiiiiiiiiiiiiiiiiiiiiiqid@gmail.com>
2025-06-19 11:12:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import random
 from aiogram import Bot, Dispatcher, types, executor
@@ -5,13 +6,13 @@ from aiogram.types import InputFile
 from aiogram.dispatcher.filters import Command
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-API_TOKEN = '7716896917:AAEBy9IR-wXNx1FfZsI9fsieZO5H1bAFUQQ'
+API_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-# Загрузка данных
+# Загрузка базы данных
 try:
     with open('data.json', 'r') as f:
         db = json.load(f)
@@ -21,3 +22,22 @@ except:
 def save_db():
     with open('data.json', 'w') as f:
         json.dump(db, f, indent=2)
+
+@dp.message_handler(commands=["start"])
+async def start(msg: types.Message):
+    uid = str(msg.from_user.id)
+    ref = msg.get_args()
+    if uid not in db["users"]:
+        db["users"][uid] = {"points": 3, "tasks": [], "refs": [], "done": []}
+        if random.random() < 0.5:
+            db["users"][uid]["points"] += 5
+            await msg.reply("Добро пожаловать! Вам начислено 3 поинта и бонус 5 поинтов 🎁")
+        else:
+            await msg.reply("Добро пожаловать! Вам начислено 3 поинта 🎉")
+        if ref and ref in db["users"] and uid not in db["users"][ref]["refs"]:
+            db["users"][ref]["points"] += 5
+            db["users"][ref]["refs"].append(uid)
+            await bot.send_message(ref, f"🎉 Новый реферал! +5 поинтов.")
+    else:
+        await msg.reply("Вы уже зарегистрированы!")
+    save_db()
